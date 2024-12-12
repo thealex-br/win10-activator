772ec04c83e135524c82cadce47c1fd69e2a86fc
theAlex <95939198+thealex-br@users.noreply.github.com>
2024-12-12 12:19:34 -0300

    Update ativador.bat

--- ativador.bat
+++ ativador.bat
@@ -1,50 +1,43 @@
-@echo off & title Ativador do Windows
+@echo off
+title Ativador do Windows
 
 :start
 cls
 echo --------------------------------------
 echo -          Ativador do Alex          -
 echo --------------------------------------
-echo                     (totalmente legal)
+echo                (totalmente legal)
 echo.
 
 echo  1. Professional
 echo  2. Home
-echo  3. Interprise
+echo  3. Enterprise
 echo  4. Education
 echo.
 
-set /p choice=Qual versao voce deseja usar? & cls & echo. & echo Clique em 'OK' para todas as caixas de dialogo que aparecerem
+set /p choice=Qual versao voce deseja usar? 
+cls
+echo. 
+echo Clique em 'OK' para todas as caixas de dialogo que aparecerem
 
-if "%choice%"=="1" goto Professional
-if "%choice%"=="2" goto Home
-if "%choice%"=="3" goto Interprise
-if "%choice%"=="4" goto Education
-if "%choice%"=="" goto end
+set "key_1=W269N-WFGWX-YVC9B-4J6C9-T83GX"
+set "key_2=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99"
+set "key_3=NPPR9-FWDCX-D2C8J-H872K-2YT43"
+set "key_4=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2"
 
-set "Professional=W269N-WFGWX-YVC9B-4J6C9-T83GX"
-set "Home=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99"
-set "Interprise=NPPR9-FWDCX-D2C8J-H872K-2YT43"
-set "Education=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2"
+if "%choice%"=="1" set "selectedKey=%key_1%"
+if "%choice%"=="2" set "selectedKey=%key_2%"
+if "%choice%"=="3" set "selectedKey=%key_3%"
+if "%choice%"=="4" set "selectedKey=%key_4%"
+if not defined selectedKey goto start
 
-:Professional
-slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-goto final
-
-:Home
-slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
-goto final
-
-:Interprise
-slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
-goto final
-
-:Education
-slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
-goto final
+slmgr /ipk %selectedKey%
 
 :final
-slmgr /skms kms8.msguides.com & cls & echo. & echo Aguarde...
+cls
+echo. 
+echo Aguarde...
+slmgr /skms kms8.msguides.com
 slmgr /ato
 goto end
 
